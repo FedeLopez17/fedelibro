bfae11b63bf6862ee77048f23243fabe9ba6cba0
FedeLopez17 <fedelopez1712@gmail.com>
2022-03-18 19:02:52 -0300

    trying to fix queries in /add

--- app.py
+++ app.py
@@ -144,6 +144,7 @@ def index():
     user_id = session["user_id"]
     username = db.execute("SELECT username FROM users WHERE id = ?", user_id)
     username = username[0]["username"]
+    print(username)
 
     if request.method == "POST":
 
